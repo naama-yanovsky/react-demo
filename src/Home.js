8ad095c01d88949717d1dc779e0deb2579c90999
Naama Yanovsky <naama.yanovsky@gmail.com>
2024-04-08 14:17:34 +0300

    done

--- src/Home.js
+++ src/Home.js
@@ -6,11 +6,11 @@ function Home() {
   return (
     <div className="home-container">
       <div className="title">כלי עזר דיגיטלי</div>
-      <div className="grey-box"></div>
-      <div className="grey-box"></div>
-      <div className="grey-box"></div>
-      <div className="grey-box"></div>
-      <div className="grey-box"></div>
+      <div className="grey-box">לי עזר דיגיטלי</div>
+      <div className="grey-box">לי עזר דיגיטלי</div>
+      <div className="grey-box">לי עזר דיגיטלי</div>
+      <div className="grey-box blue">לי עזר דיגיטלי</div>
+      <div className="grey-box blue">לי עזר דיגיטלי</div>
     </div>
   );
 }
